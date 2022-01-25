e5e60cc3944b5dd98fbed33efe3de0928f295a67
coyote <megan.metelak@gmail.com>
2022-01-25 17:09:40 -0600

    prototypes

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,7 +2,6 @@ const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
 
-console.log(new Potion());
 
 const Player = require('../lib/Player');
 
@@ -15,5 +14,24 @@ test('creates a player object', () => {
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
-      );
+    );
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
